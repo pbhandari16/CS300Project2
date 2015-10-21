7be0a7df77e1989ae25a0a621ad29b4066d0cbd8
Pratistha Bhandari <pratisthabhandari@pratisthas-macbook-pro.local>
2015-10-20 21:39:46 -0400

    Initial Commit

---
+++ Project2CS300/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2CS300
+//
+//  Created by Pratistha Bhandari on 10/20/15.
+//  Copyright © 2015 Pratistha Bhandari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
